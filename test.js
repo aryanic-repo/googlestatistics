633d7150137c75939fc6e93045e742071f5d9d77
aryanic-repo <109301497+aryanic-repo@users.noreply.github.com>
2022-09-05 14:51:28 +0430

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,13 @@
 var http = new XMLHttpRequest();
-        var url = '/user/view_docs';
-        var params = 'image_id=0';
-        http.open('POST', url, true);
-        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        http.onreadystatechange = function() {
-            if(http.readyState == 4 && http.status == 200) {
-                alert(http.responseText);
-            }
-        }
-        http.send(params);
+var url = '/user/view_docs';
+var params = 'image_id=0';
+http.open('POST', url, true);
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+http.onreadystatechange = () => {
+  if (http.readyState === http.HEADERS_RECEIVED) {
+      var cookie = http.getResponseHeader("Set-Cookie");
+      alert(cookie);
+  }
+}
 
-        http.onreadystatechange = () => {
-          if (http.readyState === http.HEADERS_RECEIVED) {
-              var cookie = http.getResponseHeader("Set-Cookie");
-              alert(cookie);
-          }
-        }
+http.send(params);
